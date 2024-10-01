24ae97d922333b6f4def01960403cc510d515230
Artem Vasilev <homelesspravitas@gmail.com>
2024-10-01 12:34:14 +0600

    btw

--- src/main/java/ru/lanit/mo/web/controller/tabulate.java
+++ src/main/java/ru/lanit/mo/web/controller/tabulate.java
@@ -5,13 +5,42 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class tabulate extends HttpServlet
 {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException
     {
+        String firstName = req.getParameter("firstName");
+        String lastName = req.getParameter("lastName");
+        String patronymic = req.getParameter("patronymic");
 
+    }
+
+    private static Connection getDBConnection()
+    {
+        Connection dbConnection = null;
+
+        try
+        {
+            Class.forName(org.postgresql.Driver.class.getName());
+        }
+        catch (ClassNotFoundException e)
+        {
+            System.out.println(e.getMessage());
+        }
+
+        try
+        {
+            dbConnection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres", "postgres", "1324");
+        }
+        catch (SQLException e)
+        {
+            System.out.println(e.getMessage());
+        }
+        return dbConnection;
     }
 }
